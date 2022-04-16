338c39e97ae92b03bac814f9c077ec8cbe2e259a
salty-max <max@jellycat.fr>
2022-04-16 22:12:11 +0200

    implements entity render system

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,5 +1,6 @@
 mod player_input;
 mod map_render;
+mod entity_render;
 
 use crate::prelude::*;
 
@@ -7,5 +8,6 @@ pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
         .build()
 }
