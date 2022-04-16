7b63e5f452970a84a02eca9b43ed5080f4041f2a
salty-max <max@jellycat.fr>
2022-04-16 21:54:07 +0200

    implements player input system

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 mod camera;
+mod components;
 mod map;
 mod map_builder;
-mod components;
 mod spawner;
 mod systems;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
-    pub use legion::*;
-    pub use legion::world::SubWorld;
     pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::camera::*;
+    pub use crate::components::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
-    pub use crate::components::*;
     pub use crate::spawner::*;
     pub use crate::systems::*;
 }
@@ -27,7 +27,7 @@ use prelude::*;
 struct State {
     ecs: World,
     resources: Resources,
-    systems: Schedule
+    systems: Schedule,
 }
 
 impl State {
@@ -44,7 +44,7 @@ impl State {
         Self {
             ecs,
             resources,
-            systems: build_scheduler()
+            systems: build_scheduler(),
         }
     }
 }
